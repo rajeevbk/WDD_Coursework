ad0de29d128502de21b52417a197a3f884139755
Isala Piyarisi <code@isala.me>
2019-07-01 11:58:51 +0530

    Added Font Size Changer

--- static/js/main.js
+++ static/js/main.js
@@ -6,3 +6,12 @@ setTimeout(function(){
 setTimeout(function(){
         document.querySelector("#splash-screen").remove();
 }, 3950);
+
+function changeGlobalFontSize(step) {
+    if(document.body.style.fontSize){
+        document.body.style.fontSize = parseFloat(document.body.style.fontSize.replace('em', '')) + step + 'em';
+    }
+    else{
+        document.body.style.fontSize = 1 + step + 'em';
+    }
+}
