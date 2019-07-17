8dbd1bc732eb40b087ababbb669035fed969f2e0
demnofocus <47452568+demnofocus@users.noreply.github.com>
2019-07-18 00:08:23 +0530

    gallery
    kevin gallery pg and site map half done file

---
+++ Final Project/static/js/gallery.js
@@ -0,0 +1,24 @@
+function myFunction(imgs) {
+                      var expandImg = document.getElementById("expandedImg"); 
+                      var imgText = document.getElementById("imgtext");
+                      var imgdetails =document.getElementById("imgdescription")
+                      expandImg.src = imgs.src;
+                      imgText.innerHTML = imgs.alt;
+                      imgdetails.innerHTML=imgs.title;
+                      expandImg.parentElement.style.display = "grid";
+                    }
+function selectBackground(){
+                    // Find selector and pull it's selection
+                    var BGselect = document.getElementById('BGColor');
+                    // Determine your target color (default 0 index to transparent)
+                    var BGcolor = (BGselect.selectedIndex != 0) ? BGselect.options[BGselect.selectedIndex].value:"white";
+                    // Update results area
+                    document.getElementById("imgdescription").style.backgroundColor = BGcolor;
+                    }
+function selectText(){
+                    // Find selector and pull it's selection
+                    var TXTselect = document.getElementById('TxtColor');
+                    // Determine target color (default 0 index to transparent)
+                    var TXTcolor = (TXTselect.selectedIndex != 0) ? TXTselect.options[TXTselect.selectedIndex].value:"Black";
+                    document.getElementById("imgdescription").style.color = TXTcolor;
+                    }
